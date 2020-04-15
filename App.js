8f5d40d2dd2eee72b2752830be95e1d3dd6f6abc
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-15 20:48:30 -0300

    adicionando método de limpar memória da calculadora

--- App.js
+++ App.js
@@ -12,6 +12,10 @@ export default class App extends Component {
     this.setState({displayValue: n});
   };
 
+  clearMemory = () => {
+    this.setState({displayValue: '0'});
+  };
+
   render() {
     return (
       <View style={styles.container}>
