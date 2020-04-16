f096bc60ed7c55f5a8cfab97ff4907dd3bba4476
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-15 21:08:30 -0300

    passando método setOperation para todas as labels de operação

--- App.js
+++ App.js
@@ -28,18 +28,18 @@ export default class App extends Component {
           <Button label="7" onClick={() => this.addDigit(7)} />
           <Button label="8" onClick={() => this.addDigit(8)} />
           <Button label="9" onClick={() => this.addDigit(9)} />
-          <Button label="*" />
+          <Button label="*" operation onClick={() => this.setOperation('*')} />
           <Button label="4" onClick={() => this.addDigit(4)} />
           <Button label="5" onClick={() => this.addDigit(5)} />
           <Button label="6" onClick={() => this.addDigit(6)} />
-          <Button label="-" />
+          <Button label="-" operation onClick={() => this.setOperation('-')} />
           <Button label="1" onClick={() => this.addDigit(1)} />
           <Button label="2" onClick={() => this.addDigit(2)} />
           <Button label="3" onClick={() => this.addDigit(3)} />
-          <Button label="+" />
+          <Button label="+" operation onClick={() => this.setOperation('+')} />
           <Button label="0" onClick={() => this.addDigit(0)} />
           <Button label="." />
-          <Button label="=" />
+          <Button label="=" operation onClick={() => this.setOperation('=')} />
         </View>
       </View>
     );
