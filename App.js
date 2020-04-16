c43fd162d6b4b224de9521000e938e641d172700
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-04-15 22:36:47 -0300

    correção de erro valor do digito '.'

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ export default class App extends Component {
       }
       values[1] = 0;
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         clearDisplay: !equals,
